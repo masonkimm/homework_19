ace90815a1c8f0740d5563216952e9f56fde0087
SungJin Kim <sungjinkimm@gmail.com>
2020-06-03 15:50:51 -0400

    api call revised

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,10 +6,17 @@ class Person extends Component {
   state = {
     result: {},
     search: '',
-    people: [],
-    photo:[],
-    rando: []
+    rando: [],
+    sortBy: "name",
+    sortDir: "asc"
   };
+
+  makeAPICall(sortBy){
+    if(this.state.sortDir === "asc"){
+
+    }
+    this.setState({sortBy: sortBy})
+  }
 
   // searchPerson = (query) => {
   //   API.search(query)
@@ -17,27 +24,16 @@ class Person extends Component {
   //     .catch((err) => console.log(err));
   // };
 
-  // === student === When this component mounts, search the Movie API for info
+  // === student === This will load up the api automatically when the page loads
   componentDidMount() {
     // this.searchPerson('');
-
-    // Axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-    //   console.log(res);
-    //   this.setState({ people: res.data });
-    // });
-
-    // Axios.get(`https://jsonplaceholder.typicode.com/photos`).then((result) => {
-    //   console.log(result);
-    //   this.setState({ photo: result.data });
-    // });
 
     Axios.get(`https://randomuser.me/api/?results=10`).then((resp) => {
       console.log(resp);
       this.setState({ rando: resp.data.results });
     });
-
-
   }
+
   // === student ===
   handleInputChange = (event) => {
     const name = event.target.name;
@@ -46,6 +42,7 @@ class Person extends Component {
       [name]: value,
     });
   };
+
   // === student === When the form is submitted, search the Movie API for `this.state.search`
   handleFormSubmit = (event) => {
     event.preventDefault();
@@ -55,13 +52,13 @@ class Person extends Component {
   render() {
     return (
       <>
-        <h1> Employee Directory </h1>
+        <h1> Employee Directoryy </h1>
         <form>
           <div className="form-group">
             <label htmlFor="search">Search:</label>
             <input
               onChange={this.handleInputChange}
-              value={this.state.value}
+              value={this.state.search}
               name="search"
               type="text"
               className="form-control"
@@ -74,89 +71,30 @@ class Person extends Component {
             </button>
           </div>
         </form>
+        
 
         <table className="table table-striped">
           <thead>
             <tr>
               <th> Image:</th>
-              <th> Name: </th>
+              <th onClick={() => this.makeAPICall("Name")}> Name: </th>
               <th> Email: </th>
               <th> Phone: </th>
               <th> DOB: </th>
             </tr>
           </thead>
           <tbody>
+          
+            {this.state.rando.map((person)=>(
+              <tr key={person.login.uuid}>
+                <td><img src={person.picture.thumbnail} alt=""></img></td>
+                <td>{person.name.first} {person.name.last}</td>
+                <td>{person.email}</td>
+                <td>{person.phone}</td>
+                <td>{person.dob.date}</td>
+              </tr>
+            ))}
             
-            {/* <tr>
-              <td> </td>
-              <td> Emily T. </td>
-              <td> 703 - 830 - 8300 </td>
-              <td> ETphonehome@gmail.com </td>
-              <td> 02-27-1992 </td>
-            </tr>
-            <tr>
-              <td> </td>
-              <td> {this.state.search} </td>
-              <td> </td>
-              <td> </td>
-            </tr> */}
-
-            {/* <tr>
-              <td>
-                {this.state.photo.slice(0,10).map((photo) => (
-                  <li key={photo.id}><img src={photo.thumbnailUrl}></img></li>
-                ))}
-              </td>
-              <td>
-                {this.state.people.map((person) => (
-                  <li key={person.id}>{person.name}</li>
-                ))}
-              </td>
-              <td>
-                {this.state.people.map((person) => (
-                  <li key={person.id}>{person.phone}</li>
-                ))}
-              </td>
-              <td>
-                {this.state.people.map((person) => (
-                  <li key={person.id}>{person.email}</li>
-                ))}
-              </td>
-              <td>
-                {this.state.people.map((person) => (
-                  <li key={person.id}>{person.company.name}</li>
-                ))}
-              </td>
-            </tr> */}
-            <tr>
-            <td>
-                {/* {this.state.rando.map((person) => (
-                  <li key={person.login.uuid}><img src={person.picture.thumbnail} alt=""></img></li>
-                ))} */}
-              </td>
-              <td>
-                {this.state.rando.map((person) => (
-                  <li key={person.login.uuid}>{person.name.first} {person.name.last} </li>
-                ))}
-              </td>
-              <td>
-                {this.state.rando.map((person) => (
-                  <li key={person.login.uuid}>{person.email}</li>
-                ))}
-              </td>
-              <td>
-                {this.state.rando.map((person) => (
-                  <li key={person.login.uuid}>{person.phone}</li>
-                ))}
-              </td>
-              <td>
-                {this.state.rando.map((person) => (
-                  <li key={person.login.uuid}>{person.dob.date}</li>
-                ))}
-              </td>
-              
-              
-            </tr>
           </tbody>
         </table>
       </>
